45c2372e7fc15a9b6f54a8b05937e2f227e46dd5
salohiddinovAsadbek <mrxcapitain@gmail.com>
2024-11-27 07:25:13 +0500

    last push

--- admin.js
+++ admin.js
@@ -14,6 +14,41 @@ const product = {
 };
 
 function addCard() {
-  const productCard = document.createElement("div");
-  productCard.classList.add("product-card");
+  let arr = JSON.parse(localStorage.getItem("addedProducts")) || [];
+
+  arr.push({
+    title: productTitle,
+    price: productPrice,
+    productCategory: productCategory,
+    img: productImage,
+  });
+
+  localStorage.setItem("addedProducts", JSON.stringify(arr));
 }
+
+const basketItemsContainer = document.querySelector(".basket-items");
+
+// Render Basket Items
+function renderBasket() {
+  const itemDiv = document.createElement("div");
+  itemDiv.className = "basket-item";
+  productAdd.innerHTML = `
+     <div>
+      <img src="./images/samsungs10.webp" alt="rasm bor">
+      <div class="details">
+        <h4>Samsung 10s</h4>
+        <p>Price: $980</p>
+      </div>
+      <div class="quantity-controls">
+        <button class="decrement">-</button>
+        <span>2</span>
+        <button class="increment">+</button>
+      </div>
+      <div class="price">$1960</div>
+     </div>
+    `;
+
+  basketItemsContainer.appendChild(itemDiv);
+}
+
+renderBasket();
--- app.js
+++ app.js
@@ -1,3 +1,43 @@
+// 1. sinxron va asinxron functionlar
+// 2. http va api nima.
+// 3. AJAX nima va readyState.
+// 4. Status kodlar.
+
+// console.log(1);
+// console.log(2);
+// alert("Salom");
+// console.log(3);
+
+// console.log(1);
+// console.log(2);
+
+// setTimeout(() => {
+//   console.log(3);
+// }, 2000);
+
+// console.log(4);
+
+// AJAX - asinxron javaScript and(va) XML
+
+const sorov = new XMLHttpRequest();
+
+sorov.addEventListener("readystatechange", function () {
+  // console.log(sorov, sorov.readyState);
+  // console.log(sorov.responseText);
+  // const parseData = JSON.parse(sorov.responseText);
+  // console.log(parseData.carts);
+  // console.log(sorov.response);
+
+  const parseData = JSON.parse(sorov.response);
+  const getData = parseData.products;
+  // const ff = JSON.parse(localStorage.getItem("products2")) || [];
+  localStorage.setItem("products2", JSON.stringify(getData));
+});
+
+sorov.open("GET", "https://dummyjson.com/products");
+
+sorov.send();
+
 let burger = document.getElementById("burgerHeader");
 let catalogDiscount = document.getElementById("catalogDiscount");
 let catalogCard = document.getElementById("catalogCard");
@@ -33,441 +73,7 @@ let search = document.getElementById("searchProduct");
 const wrapper1 = document.getElementById("wrapper1");
 const wrapper = document.getElementById("wrapper");
 
-const products = [
-  {
-    id: 1,
-    title: "Apple MacBook Pro 14 Inch Space Grey",
-    price: 1999.99,
-    category: "laptop",
-    description: {
-      date: 2021,
-      comfotness: "Comfotable",
-      brand: "Iphone",
-      design: "Amazing",
-    },
-    thumbnail:
-      "https://cdn.dummyjson.com/products/images/laptops/Apple%20MacBook%20Pro%2014%20Inch%20Space%20Grey/thumbnail.png",
-  },
-
-  {
-    id: 2,
-    title: "Apple Airpods",
-    price: 129.99,
-    category: "headphone",
-    description: {
-      date: 2018,
-      comfotness: "Relief",
-      brand: "Iphone",
-      design: "Amazing",
-    },
-    thumbnail:
-      "https://cdn.dummyjson.com/products/images/mobile-accessories/Apple%20Airpods/thumbnail.png",
-  },
-
-  {
-    id: 3,
-    title: "iPhone X",
-    price: 899.99,
-    category: "smartphone",
-    description: {
-      date: 2020,
-      comfotness: "Convenient",
-      brand: "Iphone",
-      design: "Spectacular",
-    },
-    thumbnail:
-      "https://cdn.dummyjson.com/products/images/smartphones/iPhone%20X/thumbnail.png",
-  },
-
-  {
-    id: 4,
-    title: "iPhone 6",
-    price: 299.99,
-    category: "smartphone",
-    description: {
-      date: 2021,
-      comfotness: "Ease",
-      brand: "Iphone",
-      design: "Brilliant",
-    },
-    thumbnail: "./images/iphone6.webp",
-  },
-
-  {
-    id: 5,
-    title: "Samsung Galaxy S8",
-    price: 499.99,
-    category: "smartphone",
-    description: {
-      date: 2019,
-      comfotness: "Satisfaction",
-      brand: "Samsung",
-      design: "Fabulous",
-    },
-    thumbnail: "./images/samsung8.webp",
-  },
-
-  {
-    id: 6,
-    title: "Huawei Matebook X Pro",
-    price: 1399.99,
-    category: "laptop",
-    description: {
-      date: 2020,
-      comfotness: "Contentment",
-      brand: "Huawei",
-      design: "Marvelous",
-    },
-    thumbnail:
-      "https://cdn.dummyjson.com/products/images/laptops/Huawei%20Matebook%20X%20Pro/thumbnail.png",
-  },
-
-  {
-    id: 7,
-    title: "Nike Air Jordan 1 Red And Black",
-    price: 149.99,
-    category: "clothes",
-    description: {
-      date: 2023,
-      comfotness: "Serenity",
-      brand: "Nike",
-      design: "Incredible",
-    },
-    thumbnail:
-      "https://cdn.dummyjson.com/products/images/mens-shoes/Nike%20Air%20Jordan%201%20Red%20And%20Black/thumbnail.png",
-  },
-
-  {
-    id: 8,
-    title: "Man Plaid Shirt",
-    price: 34.99,
-    category: "clotheMen",
-    description: {
-      date: 2024,
-      comfotness: "Relaxation",
-      brand: "Gucci",
-      design: "Fantastic",
-    },
-    thumbnail:
-      "https://cdn.dummyjson.com/products/images/mens-shirts/Man%20Plaid%20Shirt/thumbnail.png",
-  },
-
-  {
-    id: 9,
-    title: "Samsung Galaxy Tab S8 Plus Grey",
-    price: 599.99,
-    category: "tablet",
-    description: {
-      date: 2022,
-      comfotness: "Well-being",
-      brand: "Samsung",
-      design: "Outstanding",
-    },
-    thumbnail:
-      "https://cdn.dummyjson.com/products/images/tablets/Samsung%20Galaxy%20Tab%20S8%20Plus%20Grey/thumbnail.png",
-  },
-
-  {
-    id: 10,
-    title: "Sports Sneakers Off White Red",
-    price: 109.99,
-    category: "sport",
-    description: {
-      date: 2020,
-      comfotness: "Cozy",
-      brand: "Nike",
-      design: "Astonishing",
-    },
-    thumbnail:
-      "https://cdn.dummyjson.com/products/images/mens-shoes/Sports%20Sneakers%20Off%20White%20Red/thumbnail.png",
-  },
-
-  {
-    id: 11,
-    title: "Samsung Galaxy S10",
-    price: 699.99,
-    category: "smartphone",
-    description: {
-      date: 2019,
-      comfotness: "Convenience",
-      brand: "Samsung",
-      design: "Remarkable",
-    },
-    thumbnail: "./images/samsungs10.webp",
-  },
-
-  {
-    id: 12,
-    title: "Apple Airpods",
-    price: 129.99,
-    category: "headphone",
-    description: {
-      date: 2023,
-      comfotness: "Satisfaction",
-      brand: "Iphone",
-      design: "Astonishing",
-    },
-    thumbnail:
-      "https://cdn.dummyjson.com/products/images/mobile-accessories/Apple%20Airpods/thumbnail.png",
-  },
-
-  {
-    id: 13,
-    title: "Sports Sneakers Off White & Red",
-    price: 119.99,
-    category: "sport",
-    description: {
-      date: 2022,
-      comfotness: "Comfort",
-      brand: "Nike",
-      design: "Fabulous",
-    },
-    thumbnail:
-      "https://cdn.dummyjson.com/products/images/mens-shoes/Sports%20Sneakers%20Off%20White%20&%20Red/thumbnail.png",
-  },
-
-  {
-    id: 14,
-    title: "Oppo F19 Pro+",
-    price: 399.99,
-    category: "smartphone",
-    description: {
-      date: 2018,
-      comfotness: "Well-being",
-      brand: "Oppo",
-      design: "Incredible",
-    },
-    thumbnail:
-      "https://cdn.dummyjson.com/products/images/smartphones/Oppo%20F19%20Pro+/thumbnail.png",
-  },
-
-  {
-    id: 15,
-    title: "Blue & Black Check Shirt",
-    price: 29.99,
-    category: "clotheMen",
-    description: {
-      date: 2020,
-      comfotness: "Cozy",
-      brand: "Nike",
-      design: "Astonishing",
-    },
-    thumbnail:
-      "https://cdn.dummyjson.com/products/images/mens-shirts/Blue%20&%20Black%20Check%20Shirt/thumbnail.png",
-  },
-
-  {
-    id: 16,
-    title: "Huawei Matebook X Pro",
-    price: 1399.99,
-    category: "laptop",
-    description: {
-      date: 2024,
-      comfotness: "Relaxation",
-      brand: "Huawei",
-      design: "Brilliant",
-    },
-    thumbnail:
-      "https://cdn.dummyjson.com/products/images/laptops/Huawei%20Matebook%20X%20Pro/thumbnail.png",
-  },
-
-  {
-    id: 17,
-    title: "Apple MacBook Pro 14 Inch Space Grey",
-    price: 1999.99,
-    category: "laptop",
-    description: {
-      date: 2023,
-      comfotness: "Cozy",
-      brand: "Iphone",
-      design: "Fantastic",
-    },
-    thumbnail:
-      "https://cdn.dummyjson.com/products/images/laptops/Apple%20MacBook%20Pro%2014%20Inch%20Space%20Grey/thumbnail.png",
-  },
-
-  {
-    id: 18,
-    title: "Apple Airpods",
-    price: 129.99,
-    category: "smartphone",
-    description: {
-      date: 2022,
-      comfotness: "Relief",
-      brand: "Iphone",
-      design: "Brilliant",
-    },
-    thumbnail:
-      "https://cdn.dummyjson.com/products/images/mobile-accessories/Apple%20Airpods/thumbnail.png",
-  },
-
-  {
-    id: 19,
-    title: "iPhone X",
-    price: 899.99,
-    category: "smartphone",
-    description: {
-      date: 2020,
-      comfotness: "Convenient",
-      brand: "Iphone",
-      design: "Remarkable",
-    },
-    thumbnail:
-      "https://cdn.dummyjson.com/products/images/smartphones/iPhone%20X/thumbnail.png",
-  },
-  {
-    id: 20,
-    title: "T-shirt",
-    price: 1999,
-    thumbnail: "./img/clothe1.png",
-    category: "clotheMen",
-    description: "Футболка с дизайнерским принтом Super Shape 02 фиолетовая",
-    discountPrice: "25%",
-    discount: "2 499",
-  },
-  {
-    id: 21,
-    title: "T-shirt",
-    price: 3179,
-    thumbnail: "./img/clothe2.png",
-    category: "clotheMen",
-    description: {
-      date: 2019,
-      comfotness: "Contentment",
-      brand: "Nike",
-      design: "Marvelous",
-    },
-    discountPrice: "25%",
-    discount: 1999,
-  },
-  {
-    id: 22,
-    title: "Cap",
-    price: 1375,
-    thumbnail: "./img/cap1.png",
-    category: "clotheMen",
-    description: {
-      date: 2024,
-      comfotness: "Well-being",
-      brand: "Nike",
-      design: "Incredible",
-    },
-    discountPrice: "25%",
-    discount: 1999,
-  },
-
-  {
-    id: 23,
-    title: "T-shirt",
-    price: 2399,
-    category: "clotheMen",
-    description: {
-      date: 2023,
-      comfotness: "Convenient",
-      brand: "Nike",
-      design: "Spectacular",
-    },
-    thumbnail: "./img/id23.png",
-  },
-
-  {
-    id: 24,
-    title: "T-shirt",
-    price: 1375,
-    category: "clotheMen",
-    description: {
-      date: 2020,
-      comfotness: "Relaxation",
-      brand: "Nike",
-      design: "Satisfaction",
-    },
-    thumbnail: "./img/id24.png",
-  },
-
-  {
-    id: 25,
-    title: "T-shirt",
-    price: 2399,
-    category: "clotheMen",
-    description: {
-      date: 2021,
-      comfotness: "Cozy",
-      brand: "Nike",
-      design: "Astonishing",
-    },
-    thumbnail: "./img/id25.png",
-  },
-
-  {
-    id: 26,
-    title: "iphone 12",
-    price: 1375,
-    category: "smartphone",
-    description: {
-      date: 2023,
-      comfotness: "Serenity",
-      brand: "Iphone",
-      design: "Fabulous",
-    },
-    thumbnail: "./img/id26.png",
-  },
-
-  {
-    id: 27,
-    title: "iphone 12",
-    price: 1999,
-    category: "smartphone",
-    description: {
-      date: 2022,
-      comfotness: "Cozy",
-      brand: "Iphone",
-      design: "Astonishing",
-    },
-    thumbnail: "./img/id27.png",
-  },
-
-  {
-    id: 28,
-    title: "Cap",
-    price: 599,
-    category: "clotheMen",
-    description: {
-      date: 2021,
-      comfotness: "Cozy",
-      brand: "Nike",
-      design: "Brilliant",
-    },
-    thumbnail: "./img/id23.png",
-  },
-
-  {
-    id: 29,
-    title: "iphone 14",
-    price: 2399,
-    category: "smartphone",
-    description: {
-      date: 2020,
-      comfotness: "Cozy",
-      brand: "Iphone",
-      design: "Astonishing",
-    },
-    thumbnail: "./img/id29.png",
-  },
-
-  {
-    id: 30,
-    title: "iphone 13",
-    price: 3179,
-    category: "clotheMen",
-    description: {
-      date: 2020,
-      comfotness: "Comfotable",
-      brand: "iphone",
-      design: "Fabulous",
-    },
-    thumbnail: "./img/id30.png",
-  },
-];
+const products = JSON.parse(localStorage.getItem("products2"));
 
 function all() {
   products.map((product) => {
